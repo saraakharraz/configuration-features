46909eef8248f171f21cd628d32e02715181d61d
sara akharraz <s.akharraz@void.fr>
2026-03-15 15:27:06 +0000

    features module

--- drupal-dev/vendor/drupal/DrupalInstalled.php
+++ drupal-dev/vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = '099532d52c005378';
+  public const string VERSIONS_HASH = 'e56a715f3439c1cd';
 
 }
--- drupal-prod/vendor/drupal/DrupalInstalled.php
+++ drupal-prod/vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = '08fc7416bd33d256';
+  public const string VERSIONS_HASH = '4049a427807175d3';
 
 }
